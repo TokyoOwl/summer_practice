8a6e070c78cfd85d3b7984c433fa93bf025fcf89
Xinon Kun <wot291@gmail.com>
2025-07-07 20:32:23 +0300

    tests fixed, deleting of old tests code

--- tests/src/tests.cpp
+++ tests/src/tests.cpp
@@ -5,19 +5,6 @@
 #include "utils.h"
 #include "core.h"
 
-/*namespace tests{
-    void test_base(){
-        std::cout << core::base_function();
-        std::string str = "This is a base function!\n";
-        assert(core::base_function() == str);
-    } 
-
-    void test_utility(){
-        std::cout << utils::utility_function();
-        std::string str = "This is an utility function, that calls base_function(): This is a base function!\n";
-        assert(utils::utility_function() == str);
-    }
-}*/
 TEST(func_test, base_test)
 {
     TEST_COUT << core::base_function();
